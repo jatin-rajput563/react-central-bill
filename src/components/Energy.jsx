d8affcc00756bc3b983971e601c7459d92c648af
jatin-rajput563 <jkt3298@gmail.com>
2025-05-05 15:19:04 +0530

    starting-energy-section

---
+++ src/components/Energy.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import moduleName from '../images/png/'
+
+const Energy = () => {
+    return (
+        <>
+            <div className="min-h-screen">
+                <div className="max-w-[1140px] mx-auto">
+                    <div className="flex flex-row flex-wrap -mx-3 ">
+                        <div className="w-6/12">
+                        <img src={main-img} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Energy
