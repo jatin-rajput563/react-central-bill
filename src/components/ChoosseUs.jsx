2398719b4dde86c403bd60036524a1e8b5655975
jatin-rajput563 <jkt3298@gmail.com>
2025-05-06 11:49:11 +0530

    chooseUs-section-complete

--- src/components/ChoosseUs.jsx
+++ src/components/ChoosseUs.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import CommonHeadText from './common/CommonHeadText'
 import CommonPara from './common/CommonPara'
 import Starimg from '../images/svg/star.svg'
+import CommonSecHead from './common/CommonSecHead'
+import ButtonArrow from '../images/svg/ButtonArrow.svg'
+import comparisonImg from '../images/svg/comparison.svg'
+import ServiceImg from '../images/svg/service.svg'
+import TimeImg from '../images/svg/Time.svg'
+import SupportImg from '../images/svg/support.svg'
+
+
 
 const ChoosseUs = () => {
     return (
@@ -11,9 +19,39 @@ const ChoosseUs = () => {
                     <CommonHeadText HeadText="Why Choose Us for " SpanText="Utilities" HeadClass="!text-black text-center" />
                     <CommonPara text="This emphasizes both the action and the benefits, drawing in potential customers with a
                      clear value proposition. Let me know if you'd like any adjustments!" textClass="max-w-[620px] text-center mx-auto pt-[16px] leading-[150%] !text-[#00171F]" />
-                    <div className="flex gap-[24px] flex-wrap">
-                        <div className='bg-[#003459]'>
-                            <img src={Starimg} alt="" />
+                    <div className="flex flex-wrap gap-[24px] justify-center mt-[58px]">
+                        <div className='bg-[#003459] p-[40px] max-w-[400px] w-full rounded-[4px]'>
+                            <img src={Starimg} alt="img" />
+                            <CommonSecHead SecText="Tailored Recommendations" SecClass="!text-white pt-[30px]" />
+                            <CommonPara text="Our platform analyzes your unique energy needs and preferences
+                             to suggest the best utility providers, ensuring you get the most personalized options available." textClass="max-w-[344px] !leading-[150%] pt-[12px] opacity-75" />
+                            <button className='px-[26px] py-[22px] bg-white rounded-[446px] mt-[22px] hover:bg-[#003459] border-2 border-[#003459] hover:border-[black] transition-all leaner'>
+                                <img src={ButtonArrow} alt="" />
+                            </button>
+                        </div>
+                        <div className='flex flex-col gap-[24px]'>
+                            <div className='max-w-[334px] rounded-[4px] shadow-2xl border border-white hover:bg-[#003459] transition-all group p-[24px] min-h-[286px]'>
+                                <img src={comparisonImg} alt="" />
+                                <CommonSecHead SecText="Simplified Comparison Process" SecClass="pt-[18px] group-hover:text-[white]" />
+                                <CommonPara text="Our intuitive platform lets you compare the best utility providers in one place, helping you make a quick and informed decision." textClass="max-w-[286px] !leading-[150%] pt-[12px] !text-[#00171F] group-hover:text-[white]" />
+                            </div>
+                            <div className='max-w-[334px] rounded-[4px] shadow-2xl border border-white hover:bg-[#003459] transition-all group p-[24px] min-h-[286px]'>
+                                <img src={ServiceImg} alt="" />
+                                <CommonSecHead SecText="No Service Interruptions" SecClass="pt-[18px]" />
+                                <CommonPara text="We take care of the entire switching process so you can enjoy a hassle-free experience with zero downtime." textClass="max-w-[286px] !leading-[150%] pt-[12px] !text-[#00171F]" />
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-[24px]'>
+                            <div className='max-w-[334px] rounded-[4px] shadow-2xl border border-white hover:bg-[#003459] transition-all group p-[24px] min-h-[286px]'>
+                                <img src={TimeImg} alt="time-img" />
+                                <CommonSecHead SecText="Save Time and Money" SecClass="pt-[18px]" />
+                                <CommonPara text="With up-to-date plans and pricing, you can be sure you’re getting the most competitive rates available." textClass="max-w-[286px] !leading-[150%] pt-[12px] !text-[#00171F]" />
+                            </div>
+                            <div className='max-w-[334px] rounded-[4px] shadow-2xl border border-white hover:bg-[#003459] transition-all group p-[24px] min-h-[286px]'>
+                                <img src={SupportImg} alt="time-img" />
+                                <CommonSecHead SecText="Comprehensive Support" SecClass="pt-[18px]" />
+                                <CommonPara text="Whether you need assistance selecting a plan or during the switch, our dedicated support team is here to help every step of the way." textClass="max-w-[286px] !leading-[150%] pt-[12px] !text-[#00171F]" />
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/common/CommonSecHead.jsx
+++ src/components/common/CommonSecHead.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CommonSecHead = () => {
+const CommonSecHead = ({ SecClass, SecText }) => {
     return (
         <>
             <p className={`font-normal text-[24px] leading-[110%] text-black family m-0 ${SecClass}`}>{SecText}</p>
