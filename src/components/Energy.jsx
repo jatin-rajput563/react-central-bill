6b2c7d5959c57326142f2ac078e30a8b0c60fe72
jatin-rajput563 <jkt3298@gmail.com>
2025-05-06 09:55:28 +0530

    energy-section-complete

--- src/components/Energy.jsx
+++ src/components/Energy.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import mainImg from '../images/png/energy-mian-img.png'
 import CommonHeadText from './common/CommonHeadText'
+import CommonPara from './common/CommonPara'
 
 const Energy = () => {
     return (
         <>
-            <div className="min-h-screen pt-[180px]">
-                <div className="max-w-[1140px] mx-auto">
+            <div className="py-[150px]">
+                <div className="max-w-[1140px] mx-auto px-3">
                     <div className="flex flex-row flex-wrap -mx-3 ">
-                        <div className="w-6/12">
-                            <img src={mainImg} alt="main-img" />
+                        <div className="w-full min-lg:w-6/12">
+                            <img className='max-lg:mx-auto flex' src={mainImg} alt="main-img" />
                         </div>
-                        <div className="w-6/12">
-                            <CommonHeadText HeadText="Made Simple" FrontSpan="Switching Energy" HeadClass=" !text-black max-w-[409px]"/>
+                        <div className="w-full min-lg:w-6/12">
+                            <CommonHeadText HeadText="Made Simple" FrontSpan="Switching Energy" HeadClass=" !text-black max-w-[409px] max-lg:text-center max-lg:mx-auto max-lg:mt-[20px]" />
+                            <CommonPara text="Say goodbye to confusing energy plans! We simplify the process by providing clear and competitive electricity and gas recommendations. Whether you're moving or just seeking a better deal, our platform connects you with the best energy providers quickly and easily, helping you save on energy bills." textClass=" !text-[#00171F] max-w-[558px] !leading-[150%] pt-[16px] max-lg:text-center mx-auto" />
+                            <p className='font-normal family text-[24px] leading-[110%] text-[#003459] m-0 pt-[38px] max-lg:text-center mx-auto max-lg:mt-[20px]'>Benefits of Comparing Energy Plans</p>
+                            <div className='max-lg:justify-center max-lg:flex max-lg:items-center'>
+                                <ul className='list-disc pt-[28px] pl-[22px]'>
+                                    <li>
+                                        <p className='font-normal text-[16px] leading-[150%] text-[#00171F] m-0 family'>Save money by finding cheaper deals.</p>
+                                    </li>
+                                    <li>
+                                        <p className='font-normal text-[16px] leading-[150%] text-[#00171F] m-0 family'>Get better customer service.</p>
+                                    </li>
+                                    <li>
+                                        <p className='font-normal text-[16px] leading-[150%] text-[#00171F] m-0 family'>Explore greener, more sustainable energy options.</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/common/CommonButton.jsx
+++ src/components/common/CommonButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const CommonButton = ({ btnText, btnClass }) => {
+const CommonButton = ({ btnText, btnClass,WhiteSvg }) => {
     return (
         <div>
-            <button className={`font-family-primary font-semibold leading-[150%] text-white border-0 bg-[#2C49FE] py-[11px] px-[28px] rounded-3xl cursor-pointer transition-all duration-200 ease-linear hover:bg-[white] hover:text-[#2C49FE] ${btnClass}`}>{btnText}</button>
+            {/* <button className={`font-family-primary font-semibold leading-[150%] text-white border-0 bg-[#2C49FE] py-[11px] px-[28px] rounded-3xl cursor-pointer transition-all duration-200 ease-linear hover:bg-[white] hover:text-[#2C49FE] ${btnClass}`}>{btnText} <span>{WhiteSvg}</span><span>{BlackSvg}</span></button> */}
         </div>
     )
 }
