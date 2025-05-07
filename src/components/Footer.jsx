7149fcbc30b912157cee43b770452805ce92fd24
jatin-rajput563 <jkt3298@gmail.com>
2025-05-07 10:38:41 +0530

    adding-icons-in-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../images/svg/footer-logo.svg'
 import CommonPara from './common/CommonPara'
+import { FACEBOOK, INSTAGRAM, LINKDIN, TWITER } from '../utils/icon'
 
 const Footer = () => {
     return (
@@ -12,7 +13,30 @@ const Footer = () => {
                             <img src={logo} alt="logo" />
                             <CommonPara text="Ut auctor nisi mauris eu tincidunt facilisi. Diam amet sollicitudin pretium maecenas ornare lacus arcu adipiscing. " textClass="leading-[150%] max-w-[297px] opacity-75 pt-[28px]" />
                             <div>
-                                
+                                <p className='font-bold text-[20px] leading-[100%] text-white family  pt-[36px]'>Follow Us</p>
+                                <div className='mt-[16px] flex gap-[15.39px]'>
+                                    <div className='w-[33.5px] h-[33.5px] justify-center items-center flex border-[2px] border-white rounded-full'>
+                                        <a href="#" target='blank'>
+                                            <LINKDIN />
+                                        </a>
+                                    </div>
+                                    <div className='w-[33.5px] h-[33.5px] justify-center items-center flex border-[2px] border-white rounded-full'>
+                                        <a href="#" target='blank'>
+                                            <INSTAGRAM />
+                                        </a>
+                                    </div>
+                                    <div className='w-[33.5px] h-[33.5px] justify-center items-center flex border-[2px] border-white rounded-full'>
+                                        <a href="#" target='blank'>
+                                            <FACEBOOK />
+                                        </a>
+                                    </div>
+                                    <div className='w-[33.5px] h-[33.5px] justify-center items-center flex border-[2px] border-white rounded-full'>
+                                        <a href="#" target='blank'>
+                                            <TWITER />
+                                        </a>
+                                    </div>
+
+                                </div>
                             </div>
                         </a>
                     </div>
