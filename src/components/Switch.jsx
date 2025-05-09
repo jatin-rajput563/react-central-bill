ddb0012636792ca41dc0624be7d6a28f186de913
jatin-rajput563 <jkt3298@gmail.com>
2025-05-09 09:44:03 +0530

    switch-section-complete

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { IMG_DATA } from '../utils/helper';
+import CommonHeadText from './common/CommonHeadText'
+import CommonPara from './common/CommonPara'
 
 const Switch = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="max-w-[1440px] mx-auto pb-[162px]">
+                <CommonHeadText HeadText="Simple Steps To" SpanText="Switch" HeadClass="!text-black text-center " />
+                <CommonPara text="Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum" textClass="!text-[#4B4B4B] max-w-[610px] text-center mx-auto pt-[16px] pb-[58px]" />
+                <Swiper
+                    spaceBetween={0}
+                    slidesPerView={4}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 1,
+                        },
+                        640: {
+                            slidesPerView: 2,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                        },
+                    }}>
+                    {IMG_DATA.map((item, index) => (
+                        <SwiperSlide key={index} className='max-w-[360px] w-full mx-auto relative overflow-hidden group'>
+                            <p className='absolute left-1/2 -translate-x-1/2 rotate-[270.3deg] whitespace-nowrap bottom-[250px] xl:bottom-[300px] z-1 font-normal text-[30px] leading-[110%] m-0 family group-hover:top-[-200px] duration-500 ease-linear'>{item.para}</p>
+                            <div className="w-full bg-[#EAF9FFF0] absolute opacity-[94%] h-full group-hover:opacity-0 duration-500 ease-linear"></div>
+                            <img className='w-full ' src={item.img} alt={item.para} />
+                            <div className='max-w-[360px] w-full min-h-[246px] p-8 bg-[#003459] absolute bottom-[-130%] group-hover:bottom-0 duration-500 ease-linear'>
+                                <p className='font-normal text-[30px] max-lg:text-[20px] leading-[110%] text-white m-0 family'>{item.para}</p>
+                                <p className='font-normal text-[16px] max-lg:text-[14px] leading-[160%] text-white m-0 pt-[14px] max-w-[286px] family'>{item.description}</p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper >
+            </div >
+        </>
     )
 }
 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,6 +2,10 @@ import CamronImg from '../images/svg/camron.svg'
 import RonaldImg from '../images/svg/ronald.svg'
 import JayShahImg from '../images/svg/jay.svg'
 import starSvg from '../images/svg/greenBoxStar.svg';
+import img1 from "../images/png/slider-img-1.png";
+import img2 from "../images/png/slider-img-2.png";
+import img3 from "../images/png/slider-img-3.png";
+import img4 from "../images/png/slider-img-4.png";
 
 export const SLIDER_DATA = [
     {
@@ -51,4 +55,27 @@ export const QUESTIONS_DATA = [
         para: "Is there a fee for switching providers?",
         text: "Yes, but some contracts may include exit fees. We can guide you through the terms of your current plan and help you determine if switching now makes sense for you."
     },
-]
+];
+
+export const IMG_DATA = [
+    {
+        img: img1,
+        para: '1. Enter Your Location and Preferences',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.',
+    },
+    {
+        img: img2,
+        para: '2. Compare Providers',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.',
+    },
+    {
+        img: img3,
+        para: '3. Switch Easily',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.',
+    },
+    {
+        img: img4,
+        para: '4. Choose the Best Plan',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.',
+    }
+];
